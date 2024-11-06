a5b2c0872ae25a318185ffb35f4b7b7821d12e37
max123lentz <mlentz@stmesc.org>
2024-11-06 21:26:49 +0000

    max

--- lessons/00_Turtles/04a_Turtle_Tricks.py
+++ lessons/00_Turtles/04a_Turtle_Tricks.py
@@ -13,7 +13,7 @@ import turtle                           # Tell Python we want to work with the t
 turtle.setup (width=600, height=600)    # Set the size of the window
 
 tina = turtle.Turtle()                  # Create a turtle named tina
-
+tina.speed(10)
 # Use tina.forward() and tina.left() to draw a triangle
 # Make each side of the triangle a different color with 
 # tina.pencolor()
@@ -31,6 +31,80 @@ tina.circle(30)
 tina.circle(20)
 tina.circle(10)
 tina.circle(5)
+tina.circle(15)
+tina.circle(25)
+tina.circle(35)
+tina.circle(35)
+tina.circle(45)
+tina.circle(55)
+tina.circle(65)
+tina.circle(75)
+tina.circle(85)
+tina.circle(95)
+tina.circle(105)
+tina.circle(1)
+tina.circle(11)
+tina.circle(21)
+tina.circle(31)
+tina.circle(41)
+tina.circle(51)
+tina.circle(61)
+tina.circle(71)
+tina.circle(81)
+tina.circle(91)
+tina.circle(101)
+tina.circle(2)
+tina.circle(12)
+tina.circle(22)
+tina.circle(32)
+tina.circle(42)
+tina.circle(52)
+tina.circle(62)
+tina.circle(72)
+tina.circle(82)
+tina.circle(92)
+tina.circle(102)
+tina.circle(3)
+tina.circle(13)
+tina.circle(23)
+tina.circle(33)
+tina.circle(43)
+tina.circle(53)
+tina.circle(63)
+tina.circle(73)
+tina.circle(83)
+tina.circle(93)
+tina.circle(103)
+tina.circle(4)
+tina.circle(14)
+tina.circle(24)
+tina.circle(34)
+tina.circle(44)
+tina.circle(54)
+tina.circle(64)
+tina.circle(74)
+tina.circle(84)
+tina.circle(94)
+tina.circle(104)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
